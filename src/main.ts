72f29ce661a7cbb9ae079a4c47939b454f8a30d0
alpheratz0 <alpheratz99@protonmail.com>
2022-01-15 16:06:35 -0300

    feat: add helmet module

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import helmet from 'helmet';
 
 const port = 8080;
 const app = express();
+
+app.use(helmet());
 
 app.get('*', (req, res, next) => {
 	res.send('hi');
