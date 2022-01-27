9737495aa9edcb3501b0a19bbdb0afb0a3820b0a
adolfoCastelo1 <adolfocastelo99@gmail.com>
2022-01-26 21:28:08 -0300

    refactor: added a controller for post, removed the code from the router

---
+++ src/controllers/postController.ts
@@ -0,0 +1,15 @@
+export default class PostController {
+	PostController() {}
+
+	public getPostById = (req: any, res: any) => {
+		/* get post information (author, date, photos) */
+		const postId = req.params.id;
+		res.send({
+			id: postId
+		});
+	};
+
+	public getPostsByUserId = (req: any, res: any) => {};
+
+	public createNewPost = (req: any, res: any) => {};
+}
--- src/routers/api/v1/post/index.ts
+++ src/routers/api/v1/post/index.ts
@@ -1,17 +1,14 @@
 import express from 'express';
+import PostController from '../../../../controllers/postController';
+
+const postController = new PostController();
 
 const router = express.Router();
 
-router.get('/post/:id', (req, res) => {
-	/* send post information (author, date, photos) */
-	const postId = req.params.id;
-	res.send({
-		id: postId
-	});
-});
+router.get('/post/:id', postController.getPostById);
 
-router.post('/post', express.json(), (req, res) => {
-	/* create a new post */
-});
+router.get('/post/:userid', express.json(), postController.getPostsByUserId);
+
+router.post('/post', express.json(), postController.createNewPost);
 
 export default router;
