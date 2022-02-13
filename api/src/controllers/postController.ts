a83882648eb9b1ff3cb37cfaa3b61244d7e36396
adolfoCastelo1 <adolfocastelo99@gmail.com>
2022-02-13 16:42:55 -0300

    feat: createNewPost and likeDislikePost

--- api/src/controllers/postController.ts
+++ api/src/controllers/postController.ts
@@ -1,4 +1,7 @@
 import { Request, Response } from 'express';
+import { RequestWithUser } from '../common/requestWithUser';
+import User from '../models/user';
+import Post from '../models/post';
 
 export const getPostById = (req: Request, res: Response) => {
 	/* get post information (author, date, photos) */
@@ -10,6 +13,83 @@ export const getPostById = (req: Request, res: Response) => {
 
 export const getPostsByUserId = (req: Request, res: Response) => {};
 
-export const createNewPost = (req: Request, res: Response) => {
-	res.json({ ok: true });
+export const createNewPost = async (req: RequestWithUser, res: Response) => {
+	const description = req.body.description;
+	const imgUrl = req.body.imgUrl;
+	const email = req.user.email;
+	let user = await User.findOne({ email });
+	if (!user)
+		return res.status(400).json({
+			status: 'fail',
+			data: {
+				message: `There is not an user with email ${email} in the database`
+			}
+		});
+
+	const post = new Post({
+		description,
+		imgUrl,
+		owner: user._id,
+		likedBy: []
+	});
+	await post.save();
+
+	return res.status(201).json({
+		status: 'success',
+		data: {
+			message: `Post successfully created!`
+		}
+	});
+};
+
+export const likeDislikePost = async (req: RequestWithUser, res: Response) => {
+	try {
+		const userEmail = req.user.email;
+		const postId = req.body.postId;
+
+		let user = await User.findOne({ email: userEmail });
+		if (!user)
+			return res.status(400).json({
+				status: 'fail',
+				data: {
+					message: `There is not an user with email ${userEmail} in the database`
+				}
+			});
+
+		if (user.likedPosts.includes(postId)) user.likedPosts.remove(postId);
+		else user.likedPosts = [...user.likedPosts, postId];
+		user.save();
+
+		const userId = user._id;
+		let post = await Post.findById(postId);
+		if (!post)
+			return res.status(400).json({
+				status: 'fail',
+				data: {
+					message: `There is not an post with id ${postId} in the database`
+				}
+			});
+
+		if (post.likedBy.includes(userId)) post.likedBy.remove(userId);
+		else post.likedBy = [...post.likedBy, userId];
+
+		post.save();
+		return res.status(200).json({
+			status: 'success',
+			data: {
+				message: `Ok`
+			}
+		});
+	} catch (err) {
+		if (err instanceof Error) {
+			console.log(err.message);
+			console.log(err.stack);
+		}
+		return res.status(500).json({
+			status: 'fail',
+			data: {
+				message: `Internal server error`
+			}
+		});
+	}
 };
--- api/src/routers/api/v1/post/index.ts
+++ api/src/routers/api/v1/post/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import {
 	getPostById,
 	createNewPost,
+	likeDislikePost,
 	getPostsByUserId
 } from '../../../../controllers/postController';
 import { authenticateToken } from '../../../../middleware/authenticateToken';
@@ -17,5 +18,7 @@ router.get(
 );
 
 router.post('/post', [express.json(), authenticateToken], createNewPost);
+
+router.post('/post/like', [express.json(), authenticateToken], likeDislikePost);
 
 export default router;
