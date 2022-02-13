ff3b32b80e7cc53d0ff754c9c458dfd149d7b932
alpheratz0 <alpheratz99@protonmail.com>
2022-02-13 18:28:33 -0300

    feat: add the ability to create posts

--- api/src/controllers/postController.ts
+++ api/src/controllers/postController.ts
@@ -1,4 +1,7 @@
 import { Request, Response } from 'express';
+import { upload } from '../middleware/upload';
+import User from '../models/user';
+import Post from '../models/post';
 
 export const getPostById = (req: Request, res: Response) => {
 	/* get post information (author, date, photos) */
@@ -10,6 +13,55 @@ export const getPostById = (req: Request, res: Response) => {
 
 export const getPostsByUserId = (req: Request, res: Response) => {};
 
-export const createNewPost = (req: Request, res: Response) => {
-	res.json({ ok: true });
+export const createNewPost = async (req: Request, res: Response) => {
+	const description = req.body.description;
+	const email = (req as any).user.email;
+	const user = await User.findOne({ email });
+
+	if (!user) {
+		res.status(400).json({
+			status: 'fail',
+			data: {
+				message: `There is not an user with email ${email} in the database`
+			}
+		});
+
+		return;
+	}
+
+	upload.single('post_image')(req, res, async (err) => {
+		if (err) {
+			res.status(400).json({
+				status: 'fail',
+				data: {
+					message: err.message
+				}
+			});
+			return;
+		}
+
+		if (!req.file) {
+			res.status(400).json({
+				status: 'fail',
+				data: {
+					message: 'A photo is required to create a post'
+				}
+			});
+			return;
+		}
+
+		await new Post({
+			description,
+			imgUrl: req.file.filename,
+			owner: user._id,
+			likedBy: []
+		}).save();
+
+		res.status(201).json({
+			status: 'success',
+			data: {
+				message: `Post successfully created!`
+			}
+		});
+	});
 };
